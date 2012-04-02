74a2deefc8a60d0409da072f91d4fd57812f1a0a
Danielss89 <danielss89gmail.com>
2012-04-02 17:12:58 +0200

    Change-Id: Idf2b994b8936f08e63fdba6f6ec59cef86512b63

--- src/ZfcAclDoctrineMongoODM/Mapper/AclLoaderDoctrineMongo.php
+++ src/ZfcAclDoctrineMongoODM/Mapper/AclLoaderDoctrineMongo.php
@@ -1,7 +1,8 @@
 <?php
 namespace ZfcAclDoctrineMongoODM\Mapper;
 
-use ZfcAcl\Model\Mapper;
+use ZfcAcl\Model\Mapper\AclLoad as AclLoader,
+Zend\Acl\Acl as ZendAcl;
 
 class AclLoaderDoctrineMongo implements AclLoader 
 {
@@ -9,6 +10,49 @@ class AclLoaderDoctrineMongo implements AclLoader
     
     public function loadAclByRoleId(ZendAcl $acl, $roleId)
     {
+        $this->loadResources($acl);
+        $this->loadRoles($acl);
+        $this->loadRules($acl);
+    }
+    
+    protected function loadResources(ZendAcl $acl)
+    {
+        $resources = $this->getDm()->getRepository("ZfcAclDoctrineMongoODM\Document\Resource")->findAll();
+        
+        foreach($resources as $resource)
+        {
+            $parent = ($resource->getParent()) ? $resource->getParent()->getKey() : null;
+            $acl->addResource($resource->getKey(), $parent);
+        }
+    }
+    
+    protected function loadRoles(ZendAcl $acl)
+    {
+        $roles = $this->getDm()->getRepository("ZfcAclDoctrineMongoODM\Document\Role")->findAll();
+        
+        foreach($roles as $role)
+        {
+            $parents = $role->getParentsArray();
+            $acl->addRole($role->getName(), $parents);
+        }
+    }
+    
+    protected function loadRules(ZendAcl $acl)
+    {
+        $rules = $this->getDm()->getRepository("ZfcAclDoctrineMongoODM\Document\Rule")->findAll();
+        
+        foreach($rules as $rule)
+        {
+            $resource = $rule->getResource();
+            $priveleges = $resource->getPrivilegesArray();
+            if($rule->getType == "allow")
+            {
+                $acl->allow($rule->getRole()->getId(), $resource, $privileges);
+            } else
+            {
+                $acl->deny($rule->getRole()->getId(), $resource, $privileges);
+            }
+        }
     }
     
     public function getDm ()
